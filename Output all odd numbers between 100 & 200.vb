c763e5df8cd3244d2474aaae50d772812c0229c5
ahmedrao1 <35403822+ahmedrao1@users.noreply.github.com>
2018-03-30 01:35:22 +0500

    Add files via upload

---
+++ Output all odd numbers between 100 & 200.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        'M/J/17/22/9608
+        'Output all numbers between 100 and 2000
+
+        Dim Mycount As Integer
+        Mycount = 101
+        Do
+            Console.WriteLine(Mycount)
+            Mycount = Mycount + 2
+
+        Loop Until Mycount > 199
+        Console.ReadKey()
+
+    End Sub
+
+End Module
