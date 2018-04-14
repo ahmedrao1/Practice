16eee0709766bb4e8db11d43e0a1fe3ec2d546f7
ahmedrao1 <35403822+ahmedrao1@users.noreply.github.com>
2018-04-15 02:10:04 +0500

    Add files via upload

---
+++ MixUp String Manipulation.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        'MixUp
+        'swap the first two characters of 2 given strings
+        ' output with space between the two words
+
+        Dim x, y, b, z As String
+
+        x = Console.ReadLine
+        y = Console.ReadLine
+
+        b = Left(y, 2)
+        z = b & Mid(x, 3, Len(x))
+        
+
+        b = Left(x, 2)
+        z = z & " " & b & Mid(y, 3, Len(y))
+        Console.WriteLine(z)
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module
---
+++ fix_start string manipulation.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        'fix_start
+        ' turn all reoccurences of first alphabet to *
+
+        Dim S, b As String
+        Dim Ch As Char
+        Dim i As Integer
+
+        S = Console.ReadLine
+
+        Ch = Left(S, 1)
+        For i = 2 To Len(S)
+            b = Mid(S, i, 1)
+
+            If Mid(S, i, 1) = LCase(Ch) Or Mid(S, i, 1) = UCase(Ch) Then
+                b = "*"
+                Mid(S, i, 1) = b
+
+            End If
+        Next
+        Console.WriteLine(S)
+        Console.ReadKey()
+    End Sub
+
+End Module
