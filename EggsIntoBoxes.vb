580a431ea8af5260e08f2217c3dc38adfdfb3d3b
ahmedrao1 <35403822+ahmedrao1@users.noreply.github.com>
2018-05-21 20:19:06 +0500

    Add files via upload

---
+++ EggsIntoBoxes.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer
+        Dim y, z As Integer
+        x = Console.ReadLine
+        y = 0
+        z = 0
+        Call EggsIntoBoxes(x, y, z)
+        Console.WriteLine("Number of boxes are: " & y)
+        Console.Write("Eggs leftover are : " & z)
+        Console.ReadKey()
+    End Sub
+    Sub EggsIntoBoxes(ByVal NumberOfEggs As Integer, ByRef NumberOfBoxes As Integer, ByRef EggsLeftOver As Integer)
+        NumberOfBoxes = NumberOfEggs \ 6
+        EggsLeftOver = NumberOfEggs Mod 6
+
+
+    End Sub
+End Module
---
+++ isdivisible.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Boolean
+        Dim a, b As Integer
+        a = Console.ReadLine
+        b = Console.ReadLine
+
+        x = isDivisible(a, b)
+        Console.WriteLine(x)
+        Console.ReadKey()
+    End Sub
+    Function isDivisible(ByVal x As Integer, ByVal y As Integer) As Boolean
+        Dim output As Boolean
+        If x Mod y = 0 Then
+            output = True
+        ElseIf x Mod y <> 0 Then
+            output = False
+        End If
+        isDivisible = output
+    End Function
+End Module
